734a894e190a9dee27bdc78fb789da04a62eff07
ocich <ocich93@gmail.com>
2021-06-16 16:05:58 -0400

    [ADD]: global configurations added

---
+++ src/config/config.keys.ts
@@ -0,0 +1,9 @@
+export enum Configuration {
+  PORT = 'PORT',
+  DB_HOST = 'DB_HOST',
+  DB_USER = 'DB_USER',
+  DB_NAME = 'DB_NAME',
+  DB_PASSWORD = 'DB_PASSWORD',
+  DB_PORT = 'DB_PORT',
+  TOKEN_SECRET = 'TOKEN_SECRET',
+}
---
+++ src/database/database.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Pool, QueryResult } from 'pg';
+
+@Injectable()
+export class DatabaseService {
+  private readonly logger = new Logger(DatabaseService.name);
+
+  constructor(@Inject('DB_POOL') private pool: Pool) {}
+
+  public async executeQuery(
+    queryText: string,
+    values: any[] = [],
+  ): Promise<any[]> {
+    this.logger.debug(`Executing query: ${queryText} (${values})`);
+    return this.pool.query(queryText, values).then((result: QueryResult) => {
+      this.logger.debug(`Executed query, result size ${result.rows.length}`);
+      return result.rows;
+    });
+  }
+}
