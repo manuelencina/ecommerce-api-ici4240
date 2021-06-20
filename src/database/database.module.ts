8fedace834a5a1ebd8fb41a45f5036af1c48e3c9
ocich <ocich93@gmail.com>
2021-06-20 04:43:01 -0400

    [MODIFY]: database module modified

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -13,6 +13,9 @@ const databasePoolFactory = async (configService: ConfigService) => {
     database: configService.get(Configuration.DB_NAME),
     password: configService.get(Configuration.DB_PASSWORD),
     port: Number(configService.get(Configuration.DB_PORT)),
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 };
 
