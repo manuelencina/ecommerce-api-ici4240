1884fba2c23d4e1817ac87add1dc98228f78bca9
ocich <ocich93@gmail.com>
2021-06-27 20:33:06 -0400

    [MODIFY]: rating creator dto modified

--- src/ecommerce/order/dto/rating-creator.dto.ts
+++ src/ecommerce/order/dto/rating-creator.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, Max, MaxLength, Min } from 'class-validator';
 
 export class RatingCreatorDto {
   @ApiProperty()
   @IsNotEmpty()
+  @Min(1)
+  @Max(5)
   score: number;
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
+  @MaxLength(500)
   comment: string;
 }
