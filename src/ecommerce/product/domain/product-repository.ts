aea94847bc1774150dbada272b11b124ebec60e4
ocich <ocich93@gmail.com>
2021-06-27 22:48:07 -0400

    [ADD]: get products by filter added

--- src/ecommerce/product/domain/product-repository.ts
+++ src/ecommerce/product/domain/product-repository.ts
@@ -1,4 +1,5 @@
 export interface ProductRepository {
   get(criteriaId: string, criteriaType: string): Promise<any>;
   getProductById(productId: string): Promise<any>;
+  getProductByFilter(filter: string): Promise<any>;
 }
